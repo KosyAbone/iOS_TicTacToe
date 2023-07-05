73171f291a9382c2667fd9edec0d1b525c1547f7
Kosiso Abone <abonekosiso@gmail.com>
2023-07-04 23:52:59 -0400

    github update

--- Kosiso_Abone_Red_TicTacToe/Kosiso_GameModel.swift
+++ Kosiso_Abone_Red_TicTacToe/Kosiso_GameModel.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+
+class Kosiso_GameModel {
+    
+    var whoseTurn = "X";
+    var lastPlayed = "";
+    var whoWon = "";
+    var squareContents = Array(repeating: "", count: 10);
+    var noOfMovesPlayed = 0
+    
+    let winningCombinations = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7]
+    ]
+    
+    func playMove(tag: Int){
+        squareContents[tag] = whoseTurn;
+        lastPlayed = whoseTurn;
+        whoseTurn = whoseTurn == "X" ? "O" : "X"
+        noOfMovesPlayed += 1;
+    }
+    
+    func isGameFinished() -> Bool {
+        if(noOfMovesPlayed < 5){
+            return false
+        }
+      
+        for winningCombo in winningCombinations{
+//            let index1 = winningCombo[0]
+//            let index2 = winningCombo[1]
+//            let index3 = winningCombo[2]
+            
+            if(squareContents[index1] == lastPlayed
+               && squareContents[index2] == lastPlayed
+               && squareContents[index3] == lastPlayed){
+                
+                whoWon = lastPlayed;
+                return true
+            }
+        }
+        
+        if(noOfMovesPlayed == 9){
+            return true
+        }
+
+        return false
+    }
+    
+}
--- Kosiso_Abone_Red_TicTacToe/Kosiso_GameViewController.swift
+++ Kosiso_Abone_Red_TicTacToe/Kosiso_GameViewController.swift
@@ -8,14 +8,44 @@
 import UIKit
 
 class Kosiso_GameViewController: UIViewController {
+    // MARK:- Class variables
+    var theGameModel = Kosiso_GameModel()
+    var gameFinished = false;
 
+    // MARK:- Outlets
+    @IBOutlet weak var gameStateLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    // MARK: - IBActions
+    
+    @IBAction func squareTouched(_ sender: UIButton) {
+        
+        if(sender.currentTitle == "X" || sender.currentTitle == "O" || gameFinished){
+            return
+        }
+        sender.setTitle(theGameModel.whoseTurn, for: .normal)
+        theGameModel.playMove(tag: sender.tag)
+        
+        if(theGameModel.isGameFinished()){
+            let winner = theGameModel.whoWon
+            
+            if(winner.count > 0){
+                gameStateLabel.text = winner + " Won!"
+            } else{
+                gameStateLabel.text = "Draw!"
+            }
+            gameFinished = true;
+        }
+        else {
+            gameStateLabel.text = theGameModel.whoseTurn + "'s Turn"
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
